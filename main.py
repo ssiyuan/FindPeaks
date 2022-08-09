fa59165f0987df6864ce7b6cb26910a5c079d019
ssiyuan <ucapsqi@ucl.ac.uk>
2022-08-09 13:06:37 +0100

    delete unnecessary functions

--- main.py
+++ main.py
@@ -6,33 +6,13 @@ from utils import (
     process_original_data,
     plot_initial_2d,
     plot_initial_3d,
-    plot_peaks,
-    plot_maxes_in_range,
-    plot_peaks_in_range,
-    plot_peaks_in_ranges,
-    fit_gaussian,
-    fit_gaussian_full,
-    fit_lorentz_full,
-    fit_lorentz,
     summarize_data3D,
     summarize_peaks,
     summarize_comparison)
 
 
 def main():
-    # plot_peaks(x, ys)
-    # plot_peaks_in_range(x, ys, [0.725,0.875])  # data after log
-    # plot_peaks_in_range(x, ys, [1, 2.8])
-    # plot_maxes_in_range(x, ys, [1, 2.8])
-    # plot_peaks_in_ranges(x, ys, [[1, 2.8], [6, 7]])
-    # plot_peaks_in_ranges(x, ys, [0.2, 0.29])
-    # fit_gaussian(x, ys[15], [1.2, 2.5])  # original data
-    # fit_gaussian_full(x, ys, [[1.2, 2.5]]) 
-    # fit_lorentz_full(x, ys, [[1.2, 2.5], [6.2, 6.5]])
-    # try_sets(x, ys, [6.2, 6.5])
-    # try_sets(x, ys, [1.0, 3.0])
-    # plot_baseline(x, ys[14], [1.2, 2.5])
-
+    # test_data
     # data = read_csv("Test_Data/NH4OH-FAU-Practice-data.csv")
     # x, ys = process_original_data(data)
     # plot_initial_3d(x, ys)
@@ -43,6 +23,7 @@ def main():
     # summarize_peaks(data_3d)
     # summarize_comparison(x, ys[11], [1,6.5], csv_guess1, csv_guess2)
 
+    # ASCII_data
     data = read_ascii_files("ASCII_data")
     x, ys = process_original_data(data)
     plot_initial_3d(x, ys)
@@ -53,8 +34,6 @@ def main():
     summarize_peaks(data_3d)
     summarize_comparison(x, ys[8], [2, 4], ascii_guess1, ascii_guess2)
 
-
-    # print(data)
     
 
 if __name__ == "__main__":
